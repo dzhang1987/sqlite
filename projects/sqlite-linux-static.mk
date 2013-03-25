d2217ded0b3bf7e049b933ec659b7fc39d8bb1cb
Embedthis Software <dev@embedthis.com>
2013-03-25 15:59:57 -0700

    Add static projects

---
+++ projects/sqlite-linux-static.mk
@@ -0,0 +1,189 @@
+#
+#   sqlite-linux-static.mk -- Makefile to build SQLite Library for linux
+#
+
+PRODUCT            := sqlite
+VERSION            := 1.0.0
+BUILD_NUMBER       := 0
+PROFILE            := static
+ARCH               := $(shell uname -m | sed 's/i.86/x86/;s/x86_64/x64/;s/arm.*/arm/;s/mips.*/mips/')
+OS                 := linux
+CC                 := gcc
+LD                 := link
+CONFIG             := $(OS)-$(ARCH)-$(PROFILE)
+LBIN               := $(CONFIG)/bin
+
+
+ifeq ($(BIT_PACK_LIB),1)
+    BIT_PACK_COMPILER := 1
+endif
+
+BIT_PACK_COMPILER_PATH    := gcc
+BIT_PACK_LIB_PATH         := ar
+BIT_PACK_LINK_PATH        := link
+
+CFLAGS             += -fPIC   -w
+DFLAGS             += -D_REENTRANT -DBIT_FEATURE_SQLITE=1 -DPIC  $(patsubst %,-D%,$(filter BIT_%,$(MAKEFLAGS))) 
+IFLAGS             += -I$(CONFIG)/inc
+LDFLAGS            += '-Wl,--enable-new-dtags' '-Wl,-rpath,$$ORIGIN/' '-rdynamic'
+LIBPATHS           += -L$(CONFIG)/bin
+LIBS               += -lpthread -lm -lrt -ldl
+
+DEBUG              := debug
+CFLAGS-debug       := -g
+DFLAGS-debug       := -DBIT_DEBUG
+LDFLAGS-debug      := -g
+DFLAGS-release     := 
+CFLAGS-release     := -O2
+LDFLAGS-release    := 
+CFLAGS             += $(CFLAGS-$(DEBUG))
+DFLAGS             += $(DFLAGS-$(DEBUG))
+LDFLAGS            += $(LDFLAGS-$(DEBUG))
+
+BIT_ROOT_PREFIX    := 
+BIT_BASE_PREFIX    := $(BIT_ROOT_PREFIX)/usr/local
+BIT_DATA_PREFIX    := $(BIT_ROOT_PREFIX)/
+BIT_STATE_PREFIX   := $(BIT_ROOT_PREFIX)/var
+BIT_APP_PREFIX     := $(BIT_BASE_PREFIX)/lib/$(PRODUCT)
+BIT_VAPP_PREFIX    := $(BIT_APP_PREFIX)/$(VERSION)
+BIT_BIN_PREFIX     := $(BIT_ROOT_PREFIX)/usr/local/bin
+BIT_INC_PREFIX     := $(BIT_ROOT_PREFIX)/usr/local/include
+BIT_LIB_PREFIX     := $(BIT_ROOT_PREFIX)/usr/local/lib
+BIT_MAN_PREFIX     := $(BIT_ROOT_PREFIX)/usr/local/share/man
+BIT_SBIN_PREFIX    := $(BIT_ROOT_PREFIX)/usr/local/sbin
+BIT_ETC_PREFIX     := $(BIT_ROOT_PREFIX)/etc/$(PRODUCT)
+BIT_WEB_PREFIX     := $(BIT_ROOT_PREFIX)/var/www/$(PRODUCT)-default
+BIT_LOG_PREFIX     := $(BIT_ROOT_PREFIX)/var/log/$(PRODUCT)
+BIT_SPOOL_PREFIX   := $(BIT_ROOT_PREFIX)/var/spool/$(PRODUCT)
+BIT_CACHE_PREFIX   := $(BIT_ROOT_PREFIX)/var/spool/$(PRODUCT)/cache
+BIT_SRC_PREFIX     := $(BIT_ROOT_PREFIX)$(PRODUCT)-$(VERSION)
+
+
+TARGETS            += $(CONFIG)/bin/libsqlite3.a
+
+unexport CDPATH
+
+ifndef SHOW
+.SILENT:
+endif
+
+all build compile: prep $(TARGETS)
+
+.PHONY: prep
+
+prep:
+	@echo "      [Info] Use "make SHOW=1" to trace executed commands."
+	@if [ "$(CONFIG)" = "" ] ; then echo WARNING: CONFIG not set ; exit 255 ; fi
+	@if [ "$(BIT_APP_PREFIX)" = "" ] ; then echo WARNING: BIT_APP_PREFIX not set ; exit 255 ; fi
+	@[ ! -x $(CONFIG)/bin ] && mkdir -p $(CONFIG)/bin; true
+	@[ ! -x $(CONFIG)/inc ] && mkdir -p $(CONFIG)/inc; true
+	@[ ! -x $(CONFIG)/obj ] && mkdir -p $(CONFIG)/obj; true
+	@[ ! -f $(CONFIG)/inc/bit.h ] && cp projects/sqlite-linux-static-bit.h $(CONFIG)/inc/bit.h ; true
+	@[ ! -f $(CONFIG)/inc/bitos.h ] && cp src/bitos.h $(CONFIG)/inc/bitos.h ; true
+	@if ! diff $(CONFIG)/inc/bitos.h src/bitos.h >/dev/null ; then\
+		cp src/bitos.h $(CONFIG)/inc/bitos.h  ; \
+	fi; true
+	@if ! diff $(CONFIG)/inc/bit.h projects/sqlite-linux-static-bit.h >/dev/null ; then\
+		cp projects/sqlite-linux-static-bit.h $(CONFIG)/inc/bit.h  ; \
+	fi; true
+	@if [ -f "$(CONFIG)/.makeflags" ] ; then \
+		if [ "$(MAKEFLAGS)" != " ` cat $(CONFIG)/.makeflags`" ] ; then \
+			echo "   [Warning] Make flags have changed since the last build: "`cat $(CONFIG)/.makeflags`"" ; \
+		fi ; \
+	fi
+	@echo $(MAKEFLAGS) >$(CONFIG)/.makeflags
+clean:
+	rm -fr "$(CONFIG)/bin/libsqlite3.a"
+	rm -fr "$(CONFIG)/obj/sqlite.o"
+	rm -fr "$(CONFIG)/obj/sqlite3.o"
+
+clobber: clean
+	rm -fr ./$(CONFIG)
+
+
+
+#
+#   version
+#
+version: $(DEPS_1)
+	@echo 1.0.0-0
+
+#
+#   sqlite3.h
+#
+$(CONFIG)/inc/sqlite3.h: $(DEPS_2)
+	@echo '      [Copy] $(CONFIG)/inc/sqlite3.h'
+	mkdir -p "$(CONFIG)/inc"
+	cp src/sqlite3.h $(CONFIG)/inc/sqlite3.h
+
+#
+#   bit.h
+#
+$(CONFIG)/inc/bit.h: $(DEPS_3)
+	@echo '      [Copy] $(CONFIG)/inc/bit.h'
+
+#
+#   sqlite.o
+#
+DEPS_4 += $(CONFIG)/inc/bit.h
+DEPS_4 += $(CONFIG)/inc/sqlite3.h
+
+$(CONFIG)/obj/sqlite.o: \
+    src/sqlite.c $(DEPS_4)
+	@echo '   [Compile] src/sqlite.c'
+	$(CC) -c -o $(CONFIG)/obj/sqlite.o -fPIC $(DFLAGS) $(IFLAGS) src/sqlite.c
+
+#
+#   sqlite3.o
+#
+DEPS_5 += $(CONFIG)/inc/bit.h
+DEPS_5 += $(CONFIG)/inc/sqlite3.h
+
+$(CONFIG)/obj/sqlite3.o: \
+    src/sqlite3.c $(DEPS_5)
+	@echo '   [Compile] src/sqlite3.c'
+	$(CC) -c -o $(CONFIG)/obj/sqlite3.o -fPIC $(DFLAGS) $(IFLAGS) src/sqlite3.c
+
+#
+#   libsqlite3
+#
+DEPS_6 += $(CONFIG)/inc/sqlite3.h
+DEPS_6 += $(CONFIG)/obj/sqlite.o
+DEPS_6 += $(CONFIG)/obj/sqlite3.o
+
+$(CONFIG)/bin/libsqlite3.a: $(DEPS_6)
+	@echo '      [Link] libsqlite3'
+	ar -cr $(CONFIG)/bin/libsqlite3.a $(CONFIG)/obj/sqlite.o $(CONFIG)/obj/sqlite3.o
+
+#
+#   stop
+#
+stop: $(DEPS_7)
+
+#
+#   installBinary
+#
+installBinary: $(DEPS_8)
+
+#
+#   start
+#
+start: $(DEPS_9)
+
+#
+#   install
+#
+DEPS_10 += stop
+DEPS_10 += installBinary
+DEPS_10 += start
+
+install: $(DEPS_10)
+	
+
+#
+#   uninstall
+#
+DEPS_11 += stop
+
+uninstall: $(DEPS_11)
+
---
+++ projects/sqlite-vxworks-static.mk
@@ -0,0 +1,194 @@
+#
+#   sqlite-vxworks-static.mk -- Makefile to build SQLite Library for vxworks
+#
+
+PRODUCT            := sqlite
+VERSION            := 1.0.0
+BUILD_NUMBER       := 0
+PROFILE            := static
+ARCH               := $(shell uname -m | sed 's/i.86/x86/;s/x86_64/x64/;s/arm.*/arm/;s/mips.*/mips/')
+OS                 := vxworks
+CC                 := cc$(subst x86,pentium,$(ARCH))
+LD                 := link
+CONFIG             := $(OS)-$(ARCH)-$(PROFILE)
+LBIN               := $(CONFIG)/bin
+
+
+ifeq ($(BIT_PACK_LIB),1)
+    BIT_PACK_COMPILER := 1
+endif
+
+BIT_PACK_COMPILER_PATH    := cc$(subst x86,pentium,$(ARCH))
+BIT_PACK_LIB_PATH         := ar
+BIT_PACK_LINK_PATH        := link
+BIT_PACK_VXWORKS_PATH     := $(WIND_BASE)
+
+export WIND_BASE := $(WIND_BASE)
+export WIND_HOME := $(WIND_BASE)/..
+export WIND_PLATFORM := $(WIND_PLATFORM)
+
+CFLAGS             += -fno-builtin -fno-defer-pop -fvolatile -w
+DFLAGS             += -D_REENTRANT -DVXWORKS -DRW_MULTI_THREAD -D_GNU_TOOL -DBIT_FEATURE_SQLITE=1 -DCPU=PENTIUM $(patsubst %,-D%,$(filter BIT_%,$(MAKEFLAGS))) 
+IFLAGS             += -I$(CONFIG)/inc -I$(WIND_BASE)/target/h -I$(WIND_BASE)/target/h/wrn/coreip
+LDFLAGS            += '-Wl,-r'
+LIBPATHS           += -L$(CONFIG)/bin
+LIBS               += 
+
+DEBUG              := debug
+CFLAGS-debug       := -g
+DFLAGS-debug       := -DBIT_DEBUG
+LDFLAGS-debug      := -g
+DFLAGS-release     := 
+CFLAGS-release     := -O2
+LDFLAGS-release    := 
+CFLAGS             += $(CFLAGS-$(DEBUG))
+DFLAGS             += $(DFLAGS-$(DEBUG))
+LDFLAGS            += $(LDFLAGS-$(DEBUG))
+
+BIT_ROOT_PREFIX    := deploy
+BIT_BASE_PREFIX    := $(BIT_ROOT_PREFIX)
+BIT_DATA_PREFIX    := $(BIT_VAPP_PREFIX)
+BIT_STATE_PREFIX   := $(BIT_VAPP_PREFIX)
+BIT_BIN_PREFIX     := $(BIT_VAPP_PREFIX)
+BIT_INC_PREFIX     := $(BIT_VAPP_PREFIX)/inc
+BIT_LIB_PREFIX     := $(BIT_VAPP_PREFIX)
+BIT_MAN_PREFIX     := $(BIT_VAPP_PREFIX)
+BIT_SBIN_PREFIX    := $(BIT_VAPP_PREFIX)
+BIT_ETC_PREFIX     := $(BIT_VAPP_PREFIX)
+BIT_WEB_PREFIX     := $(BIT_VAPP_PREFIX)/web
+BIT_LOG_PREFIX     := $(BIT_VAPP_PREFIX)
+BIT_SPOOL_PREFIX   := $(BIT_VAPP_PREFIX)
+BIT_CACHE_PREFIX   := $(BIT_VAPP_PREFIX)
+BIT_APP_PREFIX     := $(BIT_BASE_PREFIX)
+BIT_VAPP_PREFIX    := $(BIT_APP_PREFIX)
+BIT_SRC_PREFIX     := $(BIT_ROOT_PREFIX)/usr/src/$(PRODUCT)-$(VERSION)
+
+
+TARGETS            += $(CONFIG)/bin/libsqlite3.a
+
+unexport CDPATH
+
+ifndef SHOW
+.SILENT:
+endif
+
+all build compile: prep $(TARGETS)
+
+.PHONY: prep
+
+prep:
+	@echo "      [Info] Use "make SHOW=1" to trace executed commands."
+	@if [ "$(CONFIG)" = "" ] ; then echo WARNING: CONFIG not set ; exit 255 ; fi
+	@if [ "$(BIT_APP_PREFIX)" = "" ] ; then echo WARNING: BIT_APP_PREFIX not set ; exit 255 ; fi
+	@[ ! -x $(CONFIG)/bin ] && mkdir -p $(CONFIG)/bin; true
+	@[ ! -x $(CONFIG)/inc ] && mkdir -p $(CONFIG)/inc; true
+	@[ ! -x $(CONFIG)/obj ] && mkdir -p $(CONFIG)/obj; true
+	@[ ! -f $(CONFIG)/inc/bit.h ] && cp projects/sqlite-vxworks-static-bit.h $(CONFIG)/inc/bit.h ; true
+	@[ ! -f $(CONFIG)/inc/bitos.h ] && cp src/bitos.h $(CONFIG)/inc/bitos.h ; true
+	@if ! diff $(CONFIG)/inc/bitos.h src/bitos.h >/dev/null ; then\
+		cp src/bitos.h $(CONFIG)/inc/bitos.h  ; \
+	fi; true
+	@if ! diff $(CONFIG)/inc/bit.h projects/sqlite-vxworks-static-bit.h >/dev/null ; then\
+		cp projects/sqlite-vxworks-static-bit.h $(CONFIG)/inc/bit.h  ; \
+	fi; true
+	@if [ -f "$(CONFIG)/.makeflags" ] ; then \
+		if [ "$(MAKEFLAGS)" != " ` cat $(CONFIG)/.makeflags`" ] ; then \
+			echo "   [Warning] Make flags have changed since the last build: "`cat $(CONFIG)/.makeflags`"" ; \
+		fi ; \
+	fi
+	@echo $(MAKEFLAGS) >$(CONFIG)/.makeflags
+clean:
+	rm -fr "$(CONFIG)/bin/libsqlite3.a"
+	rm -fr "$(CONFIG)/obj/sqlite.o"
+	rm -fr "$(CONFIG)/obj/sqlite3.o"
+
+clobber: clean
+	rm -fr ./$(CONFIG)
+
+
+
+#
+#   version
+#
+version: $(DEPS_1)
+	@echo 1.0.0-0
+
+#
+#   sqlite3.h
+#
+$(CONFIG)/inc/sqlite3.h: $(DEPS_2)
+	@echo '      [Copy] $(CONFIG)/inc/sqlite3.h'
+	mkdir -p "$(CONFIG)/inc"
+	cp src/sqlite3.h $(CONFIG)/inc/sqlite3.h
+
+#
+#   bit.h
+#
+$(CONFIG)/inc/bit.h: $(DEPS_3)
+	@echo '      [Copy] $(CONFIG)/inc/bit.h'
+
+#
+#   sqlite.o
+#
+DEPS_4 += $(CONFIG)/inc/bit.h
+DEPS_4 += $(CONFIG)/inc/sqlite3.h
+
+$(CONFIG)/obj/sqlite.o: \
+    src/sqlite.c $(DEPS_4)
+	@echo '   [Compile] src/sqlite.c'
+	$(CC) -c -o $(CONFIG)/obj/sqlite.o -fno-builtin -fno-defer-pop -fvolatile $(DFLAGS) $(IFLAGS) src/sqlite.c
+
+#
+#   sqlite3.o
+#
+DEPS_5 += $(CONFIG)/inc/bit.h
+DEPS_5 += $(CONFIG)/inc/sqlite3.h
+
+$(CONFIG)/obj/sqlite3.o: \
+    src/sqlite3.c $(DEPS_5)
+	@echo '   [Compile] src/sqlite3.c'
+	$(CC) -c -o $(CONFIG)/obj/sqlite3.o -fno-builtin -fno-defer-pop -fvolatile $(DFLAGS) $(IFLAGS) src/sqlite3.c
+
+#
+#   libsqlite3
+#
+DEPS_6 += $(CONFIG)/inc/sqlite3.h
+DEPS_6 += $(CONFIG)/obj/sqlite.o
+DEPS_6 += $(CONFIG)/obj/sqlite3.o
+
+$(CONFIG)/bin/libsqlite3.a: $(DEPS_6)
+	@echo '      [Link] libsqlite3'
+	ar -cr $(CONFIG)/bin/libsqlite3.a $(CONFIG)/obj/sqlite.o $(CONFIG)/obj/sqlite3.o
+
+#
+#   stop
+#
+stop: $(DEPS_7)
+
+#
+#   installBinary
+#
+installBinary: $(DEPS_8)
+
+#
+#   start
+#
+start: $(DEPS_9)
+
+#
+#   install
+#
+DEPS_10 += stop
+DEPS_10 += installBinary
+DEPS_10 += start
+
+install: $(DEPS_10)
+	
+
+#
+#   uninstall
+#
+DEPS_11 += stop
+
+uninstall: $(DEPS_11)
+
