32c20c1cf8a7ff5b8c929194d8792ef2349cf148
Embedthis Software <dev@embedthis.com>
2014-03-22 09:58:05 -0700

    UPGRADE: makeme updates

--- projects/sqlite-linux-static.mk
+++ projects/sqlite-linux-static.mk
@@ -9,21 +9,19 @@ ARCH                  ?= $(shell uname -m | sed 's/i.86/x86/;s/x86_64/x64/;s/arm
 CC_ARCH               ?= $(shell echo $(ARCH) | sed 's/x86/i686/;s/x64/x86_64/')
 OS                    ?= linux
 CC                    ?= gcc
-LD                    ?= ld
 CONFIG                ?= $(OS)-$(ARCH)-$(PROFILE)
 LBIN                  ?= $(CONFIG)/bin
 PATH                  := $(LBIN):$(PATH)
 
+ME_COM_VXWORKS        ?= 0
+ME_COM_WINSDK         ?= 0
 
-ME_EXT_COMPILER_PATH  ?= gcc
-ME_EXT_LIB_PATH       ?= ar
-ME_EXT_LINK_PATH      ?= ld
-ME_EXT_VXWORKS_PATH   ?= $(WIND_BASE)
 
-export WIND_HOME      ?= $(WIND_BASE)/..
+ME_COM_COMPILER_PATH  ?= gcc
+ME_COM_LIB_PATH       ?= ar
 
 CFLAGS                += -fPIC -w
-DFLAGS                += -D_REENTRANT -DPIC $(patsubst %,-D%,$(filter ME_%,$(MAKEFLAGS))) 
+DFLAGS                += -D_REENTRANT -DPIC $(patsubst %,-D%,$(filter ME_%,$(MAKEFLAGS))) -DME_COM_VXWORKS=$(ME_COM_VXWORKS) -DME_COM_WINSDK=$(ME_COM_WINSDK) 
 IFLAGS                += "-I$(CONFIG)/inc"
 LDFLAGS               += '-rdynamic' '-Wl,--enable-new-dtags' '-Wl,-rpath,$$ORIGIN/'
 LIBPATHS              += -L$(CONFIG)/bin
@@ -90,9 +88,9 @@ prep:
 	@echo $(MAKEFLAGS) >$(CONFIG)/.makeflags
 
 clean:
-	rm -f "$(CONFIG)/bin/libsql.a"
 	rm -f "$(CONFIG)/obj/sqlite.o"
 	rm -f "$(CONFIG)/obj/sqlite3.o"
+	rm -f "$(CONFIG)/bin/libsql.a"
 
 clobber: clean
 	rm -fr ./$(CONFIG)
@@ -100,15 +98,9 @@ clobber: clean
 
 
 #
-#   version
-#
-version: $(DEPS_1)
-	echo 1.0.2
-
-#
 #   sqlite3.h
 #
-$(CONFIG)/inc/sqlite3.h: $(DEPS_2)
+$(CONFIG)/inc/sqlite3.h: $(DEPS_1)
 	@echo '      [Copy] $(CONFIG)/inc/sqlite3.h'
 	mkdir -p "$(CONFIG)/inc"
 	cp src/sqlite3.h $(CONFIG)/inc/sqlite3.h
@@ -116,71 +108,62 @@ $(CONFIG)/inc/sqlite3.h: $(DEPS_2)
 #
 #   me.h
 #
-$(CONFIG)/inc/me.h: $(DEPS_3)
+$(CONFIG)/inc/me.h: $(DEPS_2)
 	@echo '      [Copy] $(CONFIG)/inc/me.h'
 
 #
 #   sqlite.o
 #
-DEPS_4 += $(CONFIG)/inc/me.h
-DEPS_4 += $(CONFIG)/inc/sqlite3.h
+DEPS_3 += $(CONFIG)/inc/me.h
+DEPS_3 += $(CONFIG)/inc/sqlite3.h
 
 $(CONFIG)/obj/sqlite.o: \
-    src/sqlite.c $(DEPS_4)
+    src/sqlite.c $(DEPS_3)
 	@echo '   [Compile] $(CONFIG)/obj/sqlite.o'
 	$(CC) -c -o $(CONFIG)/obj/sqlite.o $(CFLAGS) $(DFLAGS) $(IFLAGS) src/sqlite.c
 
 #
 #   sqlite3.o
 #
-DEPS_5 += $(CONFIG)/inc/me.h
-DEPS_5 += $(CONFIG)/inc/sqlite3.h
+DEPS_4 += $(CONFIG)/inc/me.h
+DEPS_4 += $(CONFIG)/inc/sqlite3.h
 
 $(CONFIG)/obj/sqlite3.o: \
-    src/sqlite3.c $(DEPS_5)
+    src/sqlite3.c $(DEPS_4)
 	@echo '   [Compile] $(CONFIG)/obj/sqlite3.o'
 	$(CC) -c -o $(CONFIG)/obj/sqlite3.o $(CFLAGS) $(DFLAGS) $(IFLAGS) src/sqlite3.c
 
 #
 #   libsql
 #
-DEPS_6 += $(CONFIG)/inc/sqlite3.h
-DEPS_6 += $(CONFIG)/inc/me.h
-DEPS_6 += $(CONFIG)/obj/sqlite.o
-DEPS_6 += $(CONFIG)/obj/sqlite3.o
+DEPS_5 += $(CONFIG)/inc/sqlite3.h
+DEPS_5 += $(CONFIG)/inc/me.h
+DEPS_5 += $(CONFIG)/obj/sqlite.o
+DEPS_5 += $(CONFIG)/obj/sqlite3.o
 
-$(CONFIG)/bin/libsql.a: $(DEPS_6)
+$(CONFIG)/bin/libsql.a: $(DEPS_5)
 	@echo '      [Link] $(CONFIG)/bin/libsql.a'
 	ar -cr $(CONFIG)/bin/libsql.a "$(CONFIG)/obj/sqlite.o" "$(CONFIG)/obj/sqlite3.o"
 
 #
-#   stop
-#
-stop: $(DEPS_7)
-
-#
 #   installBinary
 #
-installBinary: $(DEPS_8)
+installBinary: $(DEPS_6)
 
 #
 #   start
 #
-start: $(DEPS_9)
+start: $(DEPS_7)
 
 #
-#   install
+#   stop
 #
-DEPS_10 += stop
-DEPS_10 += installBinary
-DEPS_10 += start
-
-install: $(DEPS_10)
+stop: $(DEPS_8)
 
 #
 #   uninstall
 #
-DEPS_11 += stop
+DEPS_9 += stop
 
-uninstall: $(DEPS_11)
+uninstall: $(DEPS_9)
 
